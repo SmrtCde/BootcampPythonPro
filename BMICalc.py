bb0d2369089a32b7b1924918eab27577ca8c79c0
Ben Brown <35487501-bbrowns5851@users.noreply.replit.com>
2024-07-29 03:36:50 +0000

    Initial commit

---
+++ BMICalc.py
@@ -0,0 +1,4 @@
+weight = input("Please enter your weight, in lbs: ")
+height = input("Please enter your height, in inches: ")
+
+print("Your BMI is: " + str(703 * int(weight) / int(height) ** 2))
---
+++ HowLongToLive.py
@@ -0,0 +1,6 @@
+age = input()
+years = 90 - int(age)
+months = round(years * 12)
+weeks = round(years * 52)
+days = round(years * 365)
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
