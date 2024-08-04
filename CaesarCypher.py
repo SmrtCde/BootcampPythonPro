3aca10b474401dc81ca68d5ec8e46770a28eafe1
SmrtCde <Bbrowns585@proton.me>
2024-08-04 22:25:54 +0000

    code set to function

--- CaesarCypher.py
+++ CaesarCypher.py
@@ -2,13 +2,13 @@
 
   import CaesarCyphers
 
-  # def encrypt():
+def encrypt():
 
   num = int(input("Please pick a number between 1 - 9: "))
   txt = input("Submit your text for encryption: ")
-
+  
   shiftFactor = 0
-
+  
   nlist = []
   true = []
   cypherPos = []
@@ -19,52 +19,52 @@
   aList = []
   bList = []
   cList = []
-  encryptOutput = ""
+  encrypt = ""
   b = ""
   e = ""
-
+  
   for n in range(1, num):
     nlist.append(n)
     shiftFactor = random.choice(nlist)
-
+  
   for letter in txt:
     pos = CaesarCyphers.primaryList.index(letter)
     true.append(pos)
     s = pos * int(shiftFactor)
     aList.append(s)
     encryptA = [CaesarCyphers.primaryList[x] for x in aList]
-
+  
   for a in encryptA:
     pos = CaesarCyphers.sList.index(a)
     bList.append(pos)
     encryptB = [CaesarCyphers.tList[x] for x in bList]
-
+  
   for b in encryptB:
     pos = CaesarCyphers.qList.index(b)
     cList.append(pos)
     encryptC = [CaesarCyphers.primaryList[x] for x in cList]
-
+  
   if len(str(shiftFactor)) == 1:
     b = "0"
     e = str(shiftFactor)[:1]
   else:
     b = str(shiftFactor)[:1]
     e = str(shiftFactor)[len(str(shiftFactor)) - 1:len(str(shiftFactor))]
-
-  encryptOutput += b
+  
+  encrypt += b
   for x in encryptC:
-    encryptOutput += str(x)
-  encryptOutput += e
-
-  print(f"\nText: {txt}")
-  print(f"\nshiftFactor: {shiftFactor}")
-  print(f"\nb value {b}")
-  print(f"\ne value {e}")
-  print(f"\ntrue: {true}")
-  print(f"\naList: {aList}")
-  print(f"\nencryptA: {encryptA}")
-  print(f"\nbList: {bList}")
-  print(f"\nencryptB: {encryptB}")
-  print(f"\ncList: {cList}")
-  print(f"\nencryptC: {encryptC}")
-  print(f"\nencryptOutput: {encryptOutput}")
+    encrypt += str(x)
+  encrypt += e
+  
+  # print(f"\nText: {txt}")
+  # print(f"\nshiftFactor: {shiftFactor}")
+  # print(f"\nb value {b}")
+  # print(f"\ne value {e}")
+  # print(f"\ntrue: {true}")
+  # print(f"\naList: {aList}")
+  # print(f"\nencryptA: {encryptA}")
+  # print(f"\nbList: {bList}")
+  # print(f"\nencryptB: {encryptB}")
+  # print(f"\ncList: {cList}")
+  # print(f"\nencryptC: {encryptC}")
+  print(f"\nencryptOutput: {encrypt}")
